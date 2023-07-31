2a8e91f78a7a48ab34b12f757c211ef55f6aca98
Austin <samichpower@yahoo.com>
2023-07-31 13:03:46 -0700

    Add npc choice and display. Overwrites players X's

--- script.js
+++ script.js
@@ -14,8 +14,19 @@ function appendBoard() {
       let column = index % 3;
       board[row][column] = 'X';
       square.textContent = 'X';
+      computersTurn();
+      console.log(board);
     });
   });
 };
 
 appendBoard();
+
+function computersTurn() {
+  let index = Math.floor(Math.random() * 9);
+  let row = Math.floor(index / 3);
+  let column = index % 3;
+  board[row][column] = 'O';
+  const boardSquare = document.querySelector(`[data-index="${index}"]`);
+  boardSquare.textContent = 'O';
+}
